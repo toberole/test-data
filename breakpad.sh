0a1af5df14eacedaf0eb4296c3a559fca1ac0b2d
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-07-30 20:24:17 +0800

    note

--- breakpad.sh
+++ breakpad.sh
@@ -2,22 +2,24 @@
 read -p "please input so path:" so_file_path
 read -p "please input dmp name:" dmp_file_path
 
-cp $so_file_path crash.so
+so_file_name=$(basename ${so_file_path})
+dmp_file_name=$(basename ${dmp_file_path})
 
-cp $dmp_file_path crashdmp.dmp
+cp $so_file_path so_file_name
 
-chmod 777 crash.so
-chmod 777 crashdmp.dmp
+cp $dmp_file_path dmp_file_name
 
-touch crash.so.sym
+chmod 777 so_file_name
+chmod 777 dmp_file_name
+
+touch $so_file_name.sym
+
+chmod 777 $so_file_name.sym
 
 
-chmod 777 crash.so.sym
+./dump_syms $so_file_name.so > $so_file_name.sym
 
-
-./dump_syms crash.so > crash.so.sym
-
-head="$(head -n1 crash.so.sym)"
+head="$(head -n1 $so_file_name.sym)"
 
 echo $head
 
@@ -29,7 +31,7 @@ echo $dir2
 
 mkdir -p symbols/$dir2/$dir1
 
-mv crash.so.sym symbols/$dir2/$dir1
+mv $so_file_name.sym symbols/$dir2/$dir1
 
-./minidump_stackwalk crash_dmp.dmp symbols > crash.log
+./minidump_stackwalk $dmp_file_name symbols > crash.log
 
