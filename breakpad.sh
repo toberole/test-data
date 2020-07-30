5dc19f9a102bbf05053490ed6abd9dc375717598
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-07-30 19:09:14 +0800

    note

--- breakpad.sh
+++ breakpad.sh
@@ -3,6 +3,13 @@ read -p "please input so path:" so_file_name
 
 ./dump_syms ${so_file_name}.so > {so_file_name}.so.sym
 
-head = "$(head -n1 ${so_file_name}.so.sym)"
+head="$(head -n1 ${so_file_name}.so.sym)"
 
 echo $head
+
+array=(${head// / })
+
+len = ${#array[@]}
+
+echo $len
+
