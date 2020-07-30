c36d94969a5150f1e918d645bf0fbda81fb9fe13
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-07-30 19:28:33 +0800

    note

--- breakpad.sh
+++ breakpad.sh
@@ -11,7 +11,15 @@ head="$(head -n1 ${so_file_name}.so.sym)"
 
 echo $head
 
+s1=""
+s2=""
+s3=""
+dir1=""
+dir2=""
 
+eval $(echo $head | awk '{ printf("s1=%s;s2=%s;s3=%s;dir1=%s;dir2=%s",$1,$2,,$3,$4,$5)}')
 
-echo $len
+echo $dir1
+
+echo $dir2
 
