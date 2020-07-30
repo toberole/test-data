28cd83d18157cf052a4bb5b232c83de839bcf20a
linuxer <zhouwei441300@163.com>
2020-07-30 04:22:58 -0700

    note

--- breakpad.sh
+++ breakpad.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 read -p "please input so path:" so_file_name
 
-./dump_syms ${so_file_name}.so > {so_file_name}.so.sym
+touch ${so_file_name}.so.sym
+
+chmod 777 ${so_file_name}.so.sym
+
+./dump_syms ${so_file_name}.so > ${so_file_name}.so.sym
 
 head="$(head -n1 ${so_file_name}.so.sym)"
 
 echo $head
 
-array=(${head// / })
 
-len = ${#array[@]}
 
 echo $len
 
